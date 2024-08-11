dff97af9534caf96e380cfe40e4ac61dbf6bde2e
nguye <vinguyengia60@gmail.com>
2024-08-11 23:48:02 +0700

    message

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.printf("Hello Nguyen Gia Vi!\n");
+        System.out.printf("Welcome to the DHHTTT19A!");
+    }
+}
